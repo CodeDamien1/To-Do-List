fba86574cf8e1fddd87f7fa11313440ad7c30aa2
damien lewis <damien_lewis2k12@hotmail.com>
2023-03-31 09:48:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+//Importing Components
+import Form from './components/Form.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <hi>Damien's To Do List</hi>
+        </header>
+        <Form />
     </div>
   );
 }
